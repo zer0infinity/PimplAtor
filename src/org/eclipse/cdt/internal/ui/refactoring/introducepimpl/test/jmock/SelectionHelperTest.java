f1597b7518bc65f6f6a53146bb827f86996919ba
David Tran <david.tran@hsr.ch>
2013-11-19 21:42:39 +0100

    fixed SelectionHelperText

--- src/org/eclipse/cdt/internal/ui/refactoring/introducepimpl/test/jmock/SelectionHelperTest.java
+++ src/org/eclipse/cdt/internal/ui/refactoring/introducepimpl/test/jmock/SelectionHelperTest.java
@@ -1,35 +1,32 @@
 package org.eclipse.cdt.internal.ui.refactoring.introducepimpl.test.jmock;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-
-import org.eclipse.cdt.core.dom.ast.IASTFileLocation;
 import org.eclipse.cdt.core.dom.ast.IASTNode;
-import org.eclipse.cdt.core.dom.ast.IASTNodeLocation;
 import org.eclipse.cdt.internal.ui.refactoring.utils.SelectionHelper;
+import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.text.Region;
-import org.eclipse.jface.viewers.ISelection;
 import org.jmock.Expectations;
+import org.jmock.integration.junit3.MockObjectTestCase;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Rule;
 
 @SuppressWarnings("restriction")
-public class SelectionHelperTest {
+public class SelectionHelperTest extends MockObjectTestCase {
 
 	@Rule public JUnitRuleMockery context = new JUnitRuleMockery();
 	
 	public void testIsSelectionOnExpression() {
-		final ISelection selection = context.mock(ISelection.class);
+		final ITextSelection selection = context.mock(ITextSelection.class);
 		final IASTNode expression = context.mock(IASTNode.class);
 		context.checking(new Expectations() {{
-			oneOf(selection);
-			allowing(expression).getNodeLocations();will(returnValue(IASTNodeLocation.class));
-			allowing(expression).getFileLocation();will(returnValue(IASTFileLocation.class));
+			oneOf(selection).getOffset();
+			oneOf(selection).getLength();
+			oneOf(expression).getNodeLocations();
+			allowing(expression).getFileLocation().getNodeOffset();will(returnValue(Integer.class));
+			allowing(expression).getFileLocation().getNodeLength();will(returnValue(Integer.class));
 		}});
-		
-		Region textSelection = SelectionHelper.getRegion(selection);
-		assertNotNull(textSelection);
-		boolean res = org.eclipse.cdt.internal.ui.refactoring.introducepimpl.SelectionHelper.isSelectionOnExpression(textSelection, expression);
-		assertFalse(res);
+		Region region = SelectionHelper.getRegion(selection);
+		assertNotNull(region);
+		boolean res = org.eclipse.cdt.internal.ui.refactoring.introducepimpl.SelectionHelper.isSelectionOnExpression(region, expression);
+		assertTrue(res);
 	}
 }
